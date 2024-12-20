f921515efad964174a73720053efa773b47577a8
DerFrZocker <git@derfrzocker.de>
2024-12-20 19:05:35 +0100

    refactor(myanimelist): Remove no longer needed old MyAnimeList integration code

--- anime-calendar-server/src/main/java/de/derfrzocker/anime/calendar/integration/myanimelist/IntegrationAnimeNameListResponse.java
+++
@@ -1,9 +0,0 @@
-package de.derfrzocker.anime.calendar.integration.myanimelist;
-
-import java.util.List;
-
-public record IntegrationAnimeNameListResponse(List<Data> data) {
-
-    public record Data(Node node) {}
-    public record Node(int id, String title) {}
-}
