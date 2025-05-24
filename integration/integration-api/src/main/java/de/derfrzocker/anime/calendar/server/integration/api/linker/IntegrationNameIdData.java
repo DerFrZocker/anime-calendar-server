e3d2c8ef41e2766d83d8887247e0c8e0940c96a8
DerFrZocker <git@derfrzocker.de>
2025-05-24 15:24:53 +0200

    refactor: Remove no longer necessary IntegrationNameIdData

--- integration/integration-api/src/main/java/de/derfrzocker/anime/calendar/server/integration/api/linker/IntegrationNameIdData.java
+++
@@ -1,8 +0,0 @@
-package de.derfrzocker.anime.calendar.server.integration.api.linker;
-
-import de.derfrzocker.anime.calendar.core.integration.IntegrationAnimeId;
-import java.util.List;
-
-public record IntegrationNameIdData(IntegrationAnimeId integrationAnimeId, List<String> names) {
-
-}
--- integration/integration-api/src/main/java/de/derfrzocker/anime/calendar/server/integration/dao/IntegrationAnimeNameDao.java
+++
@@ -1,9 +0,0 @@
-package de.derfrzocker.anime.calendar.server.integration.dao;
-
-import de.derfrzocker.anime.calendar.server.integration.api.linker.IntegrationNameIdData;
-import java.util.Set;
-
-public interface IntegrationAnimeNameDao {
-
-    Set<IntegrationNameIdData> getAllAnimes();
-}
